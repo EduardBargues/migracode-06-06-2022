492d200a21ad4a7709bbc9ddd842d07fa9faf040
eduard <eduardbargues@gmail.com>
2022-07-06 21:12:09 +0200

    endpoints hechos en clase

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+const { Pool } = require("pg");
+const poolInfo = require("./pool.json");
+const pool = new Pool(poolInfo);
+
+const getApi = require("./api");
+const api = getApi({ pool });
+
+// MIDDLEWARES
+app.use(express.json());
+app.get("/hotels", api.getHotels);
+app.get("/hotels/:hotelId", api.getHotelById);
+app.delete("/hotels/:hotelId", api.deleteHotelById);
+app.post("/hotels", api.saveHotel);
+app.get("/customers/:customerId", api.getHotelById);
+app.patch("/customers/:customerId", api.updateCustomerEmail);
+app.delete("/customers/:customerId", api.deleteCustomerById);
+
+// ARRANCAR SERVIDOR
+const port = 3000;
+app.listen(port, () => console.log(`http://localhost:${port}/hotels`));
